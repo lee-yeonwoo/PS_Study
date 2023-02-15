61c6249170bc5df83d54ec31a594a9c8d5873a1e
yeonwoo <y8551472@gmail.com>
2023-02-15 12:46:13 +0900

    행렬 선언

---
+++ Silver/DFS BFS.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+N,M,V = map(int,input().split())
+
+
+# In[2]:
+
+
+graph=[[0]*(N+1) 
+       for i in range(N+1)]
+print(graph)
+
+
+# In[ ]:
+
+
+
+
+
